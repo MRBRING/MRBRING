5af40be74df28f321a773ead72df4a50c1634c8b
WarshaNushan <129195361+WarshaNushan@users.noreply.github.com>
2024-08-29 19:23:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/d6xsG7k/Presentation1-2.jpg" 
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/d6xsG7k/Presentation1-2.jpg"
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLO, I AM MRBRING IAM ALIVE NOW💚"  
 };
